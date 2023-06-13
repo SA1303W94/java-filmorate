1f0452678fc6156e85b6efb473f6a8b0b4378226
SA1303W94 <sa1303w94@yandex.ru>
2023-06-13 17:16:52 +0300

    9 спринт add controllers to films and users

---
+++ src/main/java/ru/yandex/practicum/filmorate/controller/UserController.java
@@ -0,0 +1,42 @@
+package ru.yandex.practicum.filmorate.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import ru.yandex.practicum.filmorate.dao.UserRepository;
+import ru.yandex.practicum.filmorate.exception.ValidationException;
+import ru.yandex.practicum.filmorate.model.User;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping
+@Validated
+@Slf4j
+public class UserController {
+    private final UserRepository repository = new UserRepository();
+
+    @GetMapping("/users")
+    public List<User> getUsers() { //    получение списка всех пользователей.
+        return repository.AllUsers();
+    }
+
+    @PostMapping(value = "/users") //    создание пользователя;
+    @ResponseStatus(HttpStatus.CREATED)
+    public User saveUser(@Valid @RequestBody User user) throws ValidationException {
+        log.info("Create User: {} - Started", user);
+        repository.save(user);
+        log.info("Create User: {} - Finished", user);
+        return user;
+    }
+
+    @PutMapping(value = "/users") //    обновление пользователя;
+    public User update(@Valid @RequestBody User user) throws ValidationException {
+        log.info("Update User: {} - Started", user);
+        repository.save(user);
+        log.info("Update User: {} - Finished", user);
+        return user;
+    }
+}
---
+++ src/main/java/ru/yandex/practicum/filmorate/validator/RealiseDateValidator.java
@@ -0,0 +1,16 @@
+package ru.yandex.practicum.filmorate.validator;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+import java.time.LocalDate;
+
+public class RealiseDateValidator implements ConstraintValidator<RealiseDateContraint, LocalDate> {
+
+    @Override
+    public boolean isValid(LocalDate value, ConstraintValidatorContext constraintValidatorContext) {
+        if (value == null) {
+            return true;
+        }
+        return value.isAfter(LocalDate.of(1895, 12, 28));
+    }
+}
